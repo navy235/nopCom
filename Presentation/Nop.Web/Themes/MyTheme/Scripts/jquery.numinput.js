2ce04803b4eb6423eccbb47de2d86c26adb2b69a
navy235 <shenhaijunmail@163.com>
2014-12-07 13:23:19 +0800

    test2

--- Presentation/Nop.Web/Themes/MyTheme/Scripts/jquery.numinput.js
+++ Presentation/Nop.Web/Themes/MyTheme/Scripts/jquery.numinput.js
@@ -1,83 +1,82 @@
 ﻿(function ($) {
-	$.extend($.fn, {
-		numinput:
+    $.extend($.fn, {
+        numinput:
 		function (setting) {
-			if (!setting) {
-				setting = {
-					min: 1,
-					max: 9999,
-					callback: null
-				};
-			}
-			var proxy = $.proxy;
-			var ps = $.extend({
-			}, setting);
-			var that = this;
-			that.hide();
 
-			var controlId = $(that).attr('id');
-			var wrapper = $('<div class="num-container"></div>');
-			var btnreduce = $('<span data-direction="reduce" class="num-reduce num-handle disabled"></span>');
-			var inputtext = $('<em class="num-input">1</em>');
-			var btnadd = $('<span data-direction="add" class="num-add num-handle "></span>');
+		    var default_settings = {
+		        min: 1,
+		        max: 9999,
+		        callback: null
+		    };
 
-			that.before(wrapper.append(btnreduce)
+		    var proxy = $.proxy;
+		    var ps = $.extend(default_settings, setting);
+		    var that = this;
+		    that.hide();
+
+		    var controlId = $(that).attr('id');
+		    var wrapper = $('<div class="num-container"></div>');
+		    var btnreduce = $('<span data-direction="reduce" class="num-reduce num-handle disabled"></span>');
+		    var inputtext = $('<em class="num-input">1</em>');
+		    var btnadd = $('<span data-direction="add" class="num-add num-handle "></span>');
+
+		    that.before(wrapper.append(btnreduce)
 				.append(inputtext)
 				.append(btnadd));
 
 
-			wrapper.on('click', '.num-handle', proxy(changeNum, that, ps.callback));
+		    wrapper.on('click', '.num-handle', proxy(changeNum, that, ps.callback));
 
-			init();
+		    init();
 
-			function init() {
+		    function init() {
 
-				var currentValue = $('#' + controlId).val();
-				if (currentValue == ps.min) {
-					btnreduce.addClass('disabled');
-				} else {
-					btnreduce.removeClass('disabled');
-				}
-				if (currentValue == ps.max) {
-					btnadd.addClass('disabled');
-				} else {
-					btnadd.removeClass('disabled');
-				}
-				inputtext.text(currentValue);
-			}
+		        var currentValue = $('#' + controlId).val();
+		        if (currentValue == ps.min) {
+		            btnreduce.addClass('disabled');
+		        } else {
+		            btnreduce.removeClass('disabled');
+		        }
+		        if (currentValue == ps.max) {
+		            btnadd.addClass('disabled');
+		        } else {
+		            btnadd.removeClass('disabled');
+		        }
+		        inputtext.text(currentValue);
+		    }
 
-			function changeNum(callback, e) {
-				e.preventDefault();
-				var target = $(e.currentTarget);
+		    function changeNum(callback, e) {
+		        e.preventDefault();
+		        var target = $(e.currentTarget);
 
-				var direction = target.data('direction');
-				var currentValue = $('#' + controlId).val();
-				if (target.hasClass('disabled')) {
-					return false;
-				} else {
-					if (direction == "add") {
-						currentValue++;
-					} else {
-						currentValue--;
-					}
-				}
-				if (currentValue == ps.min) {
-					btnreduce.addClass('disabled');
-				} else {
-					btnreduce.removeClass('disabled');
-				}
-				if (currentValue == ps.max) {
-					btnadd.addClass('disabled');
-				} else {
-					btnadd.removeClass('disabled');
-				}
-				$('#' + controlId).val(currentValue);
-				inputtext.text(currentValue);
-				if (callback) {
-					callback();
-				}
-			}
+		        var direction = target.data('direction');
+		        var currentValue = $('#' + controlId).val();
+		        if (target.hasClass('disabled')) {
+		            return false;
+		        } else {
+		            if (direction == "add") {
+		                currentValue++;
+		            } else {
+		                currentValue--;
+		            }
+		        }
+		        if (currentValue == ps.min) {
+		            btnreduce.addClass('disabled');
+		        } else {
+		            btnreduce.removeClass('disabled');
+		        }
+		        if (currentValue == ps.max) {
+		            btnadd.addClass('disabled');
+		        } else {
+		            btnadd.removeClass('disabled');
+		        }
+		        $('#' + controlId).val(currentValue);
+		        inputtext.text(currentValue);
+		        if (callback) {
+		            callback();
+		        }
+		    }
 		}
-	});
+    });
 })(jQuery)
 
